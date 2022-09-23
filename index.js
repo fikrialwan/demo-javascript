b96f94a84e05af3d80a3e528ddadfa728e3691a4
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-23 22:24:08 +0700

    feat: add function javascript to get number, choose even or odd and show number

---
+++ index.js
@@ -0,0 +1,38 @@
+const getLastNumber = () => {
+  const lastNumber = Number(prompt("Input the last number"));
+  if (lastNumber > 0) {
+    return lastNumber;
+  } else {
+    getLastNumber();
+  }
+}
+
+const getEvenOrOdd = () => {
+  const evenOrOdd = prompt("Choose even or odd").toLowerCase();
+  if (evenOrOdd === "odd" || evenOrOdd === "even") {
+    return evenOrOdd;
+  } else {
+    getEvenOrOdd();
+  }
+}
+
+const showNumber = (lastNumber, evenOrOdd) => {
+  console.log(`${evenOrOdd} number from 1 = ${lastNumber}`);
+  if (evenOrOdd === 'odd') {
+    for (let number = 1; number <= lastNumber; number++) {
+      if (number % 2 !== 0) {
+        console.log(number);
+      }
+    }
+  } else {
+    for (let number = 1; number <= lastNumber; number++) {
+      if (number % 2 === 0) {
+        console.log(number);
+      }
+    }
+  }
+}
+
+const lastNumber = getLastNumber();
+const evenOrOdd = getEvenOrOdd();
+showNumber(lastNumber, evenOrOdd);
