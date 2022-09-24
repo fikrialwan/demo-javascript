08276e1d181b01fb1d6ce974131c6754de5d1570
M Haidar Hanif <git@mhaidarhanif.com>
2022-09-24 21:53:25 +0700

    refactor: variable and function naming

--- index.js
+++ index.js
@@ -1,41 +1,47 @@
-const getLastNumber = () => {
-  const lastNumber = Number(prompt("Input the last number"));
-  if (lastNumber > 0) {
-    return lastNumber;
-  } else {
-    return getLastNumber();
-  }
-}
+const inputMaximumNumber = () => {
+  const maximumNumber = Number(prompt("Input the maximum number"));
 
-const getEvenOrOdd = () => {
+  if (maximumNumber > 0) {
+    return maximumNumber;
+  } else {
+    return inputMaximumNumber();
+  }
+};
+
+const chooseEvenOrOdd = () => {
   const evenOrOdd = prompt("Choose even or odd").toLowerCase();
+
   if (evenOrOdd === "odd" || evenOrOdd === "even") {
     return evenOrOdd;
   } else {
-    return getEvenOrOdd();
+    return chooseEvenOrOdd();
   }
-}
+};
 
-const showNumber = (lastNumber, evenOrOdd) => {
-  console.log(`${evenOrOdd} number from 1 - ${lastNumber}`);
-  if (evenOrOdd === 'odd') {
-    for (let number = 1; number <= lastNumber; number++) {
+const generateNumbers = (maximumNumber, evenOrOdd) => {
+  console.log(`${evenOrOdd} number from 1 - ${maximumNumber}`);
+
+  if (evenOrOdd === "odd") {
+    for (let number = 1; number <= maximumNumber; number++) {
       if (number % 2 !== 0) {
         console.log(number);
       }
     }
-  } else {
-    for (let number = 1; number <= lastNumber; number++) {
+  } else if (evenOrOdd === "even") {
+    for (let number = 1; number <= maximumNumber; number++) {
       if (number % 2 === 0) {
         console.log(number);
       }
     }
+  } else {
+    console.log("Cannot generate odd or even numbers");
   }
-}
+};
 
-const startButton = document.querySelector('button');
-startButton.addEventListener('click', () => {
-  const lastNumber = getLastNumber();
-  const evenOrOdd = getEvenOrOdd();
-  showNumber(lastNumber, evenOrOdd);
+const startButton = document.querySelector("button");
+
+startButton.addEventListener("click", () => {
+  const maximumNumber = inputMaximumNumber();
+  const evenOrOdd = chooseEvenOrOdd();
+  generateNumbers(maximumNumber, evenOrOdd);
 });
